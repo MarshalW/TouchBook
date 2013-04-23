e8b102141e4cd3b68f31e31dca892641426d75d2
Marshal Wu <marshal.wu@gmail.com>
2013-04-23 10:32:11 +0800

    获取准确的element针对屏幕坐标（针对第一屏）

--- src/com/example/touchbook/MyActivity.java
+++ src/com/example/touchbook/MyActivity.java
@@ -1,7 +1,15 @@
 package com.example.touchbook;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +19,28 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage(ConsoleMessage message) {
+                Log.d("touchbook", message.message() + " -- From line "
+                        + message.lineNumber() + " of "
+                        + message.sourceId());
+                return true;
+            }
+        });
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        ViewGroup rootView=(ViewGroup)findViewById(R.id.rootView);
+        rootView.addView(webView);
+
+        TouchView touchView=new TouchView(this);
+        touchView.setBackgroundColor(Color.TRANSPARENT);
+        rootView.addView(touchView);
     }
 }
---
+++ src/com/example/touchbook/TouchView.java
@@ -0,0 +1,47 @@
+package com.example.touchbook;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marshal
+ * Date: 13-4-22
+ * Time: 上午10:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class TouchView extends AbsoluteLayout {
+    public TouchView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(3);
+
+        paint.setColor(Color.parseColor("#70ffff00"));
+        canvas.drawRect(0, 0, 360 * 2, 70 * 2, paint);
+
+        paint.setColor(Color.parseColor("#70ffff55"));
+        canvas.drawRect(0, 70 * 2, 360 * 2, 182 * 2, paint);
+
+        paint.setColor(Color.parseColor("#70ffff10"));
+        canvas.drawRect(0, 182 * 2, 360 * 2, 409 * 2, paint);
+
+        paint.setColor(Color.parseColor("#70fff010"));
+        canvas.drawRect(0, 409 * 2, 360 * 2, 544 * 2, paint);
+//        canvas.drawRect(0, 480 * 2, 360 * 2, (560) * 2, paint);
+
+
+        paint.setColor(Color.parseColor("#70ff0000"));
+        canvas.drawRect(152 * 2, 182 * 2, 360 * 2, 323 * 2, paint);
+    }
+}
